02d624c8a80f2323f455783515182eba1def202a
Dimitri Khodak <dimitri.khodak@outlook.com>
2025-12-19 00:16:03 +0100

    change .NET-project

---
+++ MsSsisPackageFactory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace MsSsisPackageFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application application = new Application();
+            Package package = new Package();
+            application.SaveToXml("C:\\Temp\\DbReplicator.dtsx", package, null);
+        }
+    }
+}
